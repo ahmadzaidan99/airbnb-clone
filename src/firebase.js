69e91702d3dbb3e0c4e4b8c836720850e2087ed4
ahmadzaidan99 <zaidan.ahmad@hotmail.com>
2021-03-11 03:01:50 +0200

    Final

--- src/firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDQ45ONP5Vn3ryXwUwsfkZHpMk7-gro0bE",
-  authDomain: "airbnb-clone-48d6b.firebaseapp.com",
-  projectId: "airbnb-clone-48d6b",
-  storageBucket: "airbnb-clone-48d6b.appspot.com",
-  messagingSenderId: "735552548336",
-  appId: "1:735552548336:web:3d8d6dcc2cdc2fb342c135",
-};
